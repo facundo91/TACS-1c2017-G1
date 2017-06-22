d6adcbd162cce279ba99724e0ffef1cac05de737
Facundo Aita <facundo91@gmail.com>
2017-06-22 16:32:23 -0300

    Cool scripts

--- commit.sh
+++ commit.sh
@@ -1,12 +1,17 @@
-echo "Commit bundle.js or style.css? (y/n)"
-read commitBundle
+read -p "Commit bundle.js or style.css? (Y/n)  " commitBundle
+commitBundle=${commitBundle:-Y}
 
-if [ "$commitBundle" = "y" ]; then
+if [[ $commitBundle = [Yy] ]]; then
+  echo "Commiting files"
   git update-index --no-assume-unchanged src/main/resources/static/bundle.js
   git update-index --no-assume-unchanged src/main/resources/static/style.css
-else
+elif [[ $commitBundle = [Nn] ]]; then
+  echo "Commiting anything else"
   git update-index --assume-unchanged src/main/resources/static/bundle.js
   git update-index --assume-unchanged src/main/resources/static/style.css
+else
+  echo "I don't understand. Bye!"
+  exit 1
 fi
 
 if [[ `git status --porcelain` ]]; then
--- deploy.sh
+++ deploy.sh
@@ -3,35 +3,34 @@ FILE="logs/deploy-`date --iso-8601=seconds`.log"
 touch $FILE
 echo "[`date --iso-8601=seconds`]: Logging" >> $FILE
 cat src/main/resources/asciiart.txt
-echo "Deploy front end? (y/n)"
-read front
 
-if [ "$front" = "y" ]; then
-#Compiling the front-end
-echo 'Deploying front end'
-cd src/main/resources/static/
-sudo npm install && gulp make
-cd ../../../../
+read -p "Deploy front end? (Y/n) " front
+front=${front:-Y}
+if [[ $front = [Yy] ]]; then
+  #Compiling the front-end
+  echo 'Deploying front end'
+  cd src/main/resources/static/
+  sudo npm install && gulp make
+  cd ../../../../
 fi
 
 
-echo "Deploy back end? (y/n)"
-read back
-
-if [ "$back" = "y" ]; then
-#Install the project
-echo 'Making project'
-mvn clean install -DskipTests -P openshift
-#Move the Web Archive File to the Tomcat root
-echo 'Moving WAR to Tomcat root'
-sudo cp webapps/*.war /opt/tomcat/webapps/
-#Removing target folder
-echo 'Removing target folder'
-rm -rf target/
-#Open the project main url (Could be too early)
-echo 'Openning browser for you'
-xdg-open http://localhost:8080/
-#See logs
-echo 'Showing Logs'
-sudo less +F /opt/tomcat/logs/catalina.out
+read -p "Deploy back end? (Y/n) " back
+back=${back:-Y}
+if [[ $back = [Yy] ]]; then
+  #Install the project
+  echo 'Making project'
+  mvn clean install -DskipTests -P openshift
+  #Move the Web Archive File to the Tomcat root
+  echo 'Moving WAR to Tomcat root'
+  sudo cp webapps/*.war /opt/tomcat/webapps/
+  #Removing target folder
+  echo 'Removing target folder'
+  rm -rf target/
+  #Open the project main url (Could be too early)
+  echo 'Openning browser for you'
+  xdg-open http://localhost:8080/
+  #See logs
+  echo 'Showing Logs'
+  sudo less +F /opt/tomcat/logs/catalina.out
 fi
