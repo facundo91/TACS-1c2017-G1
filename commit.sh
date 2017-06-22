25b6622ce3c19b42f860baf6fb822b09b71adef1
Facundo Aita <facundo91@gmail.com>
2017-06-22 15:48:10 -0300

    Changing commit script

--- commit.sh
+++ commit.sh
@@ -3,5 +3,5 @@ git add --force src/main/resources/static/style.css
 git add --force src/main/resources/static/bundle.js
 echo "Enter commit comment"
 read comment
-git commit -m '`$comment`'
+git commit -m "$comment"
 git push
